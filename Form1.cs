bb3a90bd8c7c34aa38cf60d2908565f0bd9c0608
nrfx <zavalin001@ya.ru>
2026-04-01 15:09:14 +0800

    поделал на паре

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,24 @@
+using EventProcessing.Objects;
+
 namespace EventProcessing
 {
     public partial class Form1 : Form
     {
+        MyRectangle myRect;
         public Form1()
         {
             InitializeComponent();
+            myRect = new MyRectangle(0, 0, 0);
         }
 
         private void pbMain_Paint(object sender, PaintEventArgs e)
         {
             var g = e.Graphics;
-            g.DrawRectangle(new Pen(Color.Red), 200, 100, 50, 30);
-            g.FillRectangle(new SolidBrush(Color.Yellow), 200, 100, 50, 30);
+            g.Clear(Color.White);
+            /*g.FillRectangle(new SolidBrush(Color.Yellow), 200, 100, 50, 30);
+            g.DrawRectangle(new Pen(Color.Red, 2), 200, 100, 50, 30);*/
+
+            myRect.Render(g);
         }
     }
 }
---
+++ Objects/MyRectangle.cs
@@ -0,0 +1,19 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventProcessing.Objects
+{
+    class MyRectangle : BaseObject
+    {
+        public MyRectangle(float x, float y, float angle) : base(x, y, angle)
+        {
+        }
+
+        public override void Render(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(Color.Yellow), 200, 100, 50, 30);
+            g.DrawRectangle(new Pen(Color.Red, 2), 200, 100, 50, 30);
+        }
+    }
+}
